be79dad420e58ba7883dc7a99033aef63d205089
Eaknyt <deaknyt@gmail.com>
2017-09-20 18:45:33 +0200

    Add a CameraController class
    Currently, it contains just logic to move an eye and a target with the
    arrow keys.

---
+++ cameracontroller.cpp
@@ -0,0 +1,127 @@
+#include "cameracontroller.h"
+
+#include <QKeyEvent>
+#include <QMatrix4x4>
+#include <QtMath>
+
+
+CameraController::CameraController(QObject *parent) :
+    QObject(parent),
+    m_keyDirection(KeyDirection::None)
+{}
+
+CameraController::~CameraController()
+{}
+
+bool CameraController::eventFilter(QObject *obj, QEvent *e)
+{
+    const QEvent::Type eventType = e->type();
+
+    if (eventType == QEvent::KeyPress) {
+        auto keyEvent = static_cast<QKeyEvent *>(e);
+
+        keyPressEvent(keyEvent);
+
+        return false;
+    }
+    else if (eventType == QEvent::KeyRelease) {
+        auto keyEvent = static_cast<QKeyEvent *>(e);
+
+        keyReleaseEvent(keyEvent);
+
+        return false;
+    }
+
+    return QObject::eventFilter(obj, e);
+}
+
+QVector3D CameraController::computeDirectionFromKeys(const QVector3D &eye,
+                                                     const QVector3D &target,
+                                                     const QVector3D &up) const
+{
+    const QVector3D frontDir = (target - eye).normalized();
+    const QVector3D rightDir = QVector3D::crossProduct(frontDir, up);
+
+    QVector3D ret;
+
+    switch (m_keyDirection) {
+    case KeyDirection::None:
+        break;
+    case KeyDirection::Up:
+        ret = frontDir;
+        break;
+    case KeyDirection::Down:
+        ret = frontDir * -1;
+        break;
+    case KeyDirection::Left:
+        ret = rightDir * -1;
+        break;
+    case KeyDirection::Right:
+        ret = rightDir;
+        break;
+    }
+
+    return ret;
+}
+
+void CameraController::updateViewMatrix(QVector3D &eye,
+                                        QVector3D &target,
+                                        QVector3D &up)
+{
+    const QVector3D oldEye = eye;
+
+    const float moveAmount = (12 / 1000.0f);
+    const QVector3D newEye =
+            computeDirectionFromKeys(oldEye, target, up) * moveAmount + oldEye;
+
+    eye = newEye;
+    target += (newEye - oldEye);
+}
+
+void CameraController::keyPressEvent(QKeyEvent *event)
+{
+    // Key handling
+    const int key = event->key();
+
+    KeyDirection newDirection = KeyDirection::None;
+
+    if (key == Qt::Key_Z) {
+        newDirection = KeyDirection::Up;
+    }
+    else if (key == Qt::Key_S) {
+        newDirection = KeyDirection::Down;
+    }
+    else if (key == Qt::Key_Q) {
+        newDirection = KeyDirection::Left;
+    }
+    else if (key == Qt::Key_D) {
+        newDirection = KeyDirection::Right;
+    }
+
+    m_keyDirection = (KeyDirection) ((int) m_keyDirection | (int) newDirection);
+}
+
+void CameraController::keyReleaseEvent(QKeyEvent *event)
+{
+    const int key = event->key();
+
+    KeyDirection directionToExit = KeyDirection::None;
+
+    if (key == Qt::Key_Z) {
+        directionToExit = KeyDirection::Up;
+    }
+    else if (key == Qt::Key_S) {
+        directionToExit = KeyDirection::Down;
+    }
+    else if (key == Qt::Key_Q) {
+        directionToExit = KeyDirection::Left;
+    }
+    else if (key == Qt::Key_D) {
+        directionToExit = KeyDirection::Right;
+    }
+    else {
+        directionToExit = KeyDirection::None;
+    }
+
+    m_keyDirection = (KeyDirection) ((int) m_keyDirection & (~(int) directionToExit));
+}
---
+++ cameracontroller.h
@@ -0,0 +1,46 @@
+#ifndef CAMERACONTROLLER_H
+#define CAMERACONTROLLER_H
+
+#include <QObject>
+#include <QPoint>
+#include <QVector3D>
+
+
+class QKeyEvent;
+class QMouseEvent;
+
+
+class CameraController : public QObject
+{
+    Q_OBJECT
+
+public:
+    CameraController(QObject *parent = nullptr);
+    ~CameraController();
+
+    bool eventFilter(QObject *obj, QEvent *e) override;
+
+    void updateViewMatrix(QVector3D &eye, QVector3D &target, QVector3D &up);
+
+private:
+    void keyPressEvent(QKeyEvent* event);
+    void keyReleaseEvent(QKeyEvent* event);
+
+    QVector3D computeDirectionFromKeys(const QVector3D &eye,
+                                       const QVector3D &target,
+                                       const QVector3D &up) const;
+
+private:
+    enum KeyDirection
+    {
+        None = 0,
+        Up = 1,
+        Down = 2,
+        Right = 4,
+        Left = 8
+    };
+
+    KeyDirection m_keyDirection;
+};
+
+#endif // CAMERACONTROLLER_H
